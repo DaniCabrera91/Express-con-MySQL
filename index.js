5a9b718a681a1a61c84e406e66dcaad64cd0edbe
DaniCabrera91 <danielcabreravazquez91@gmail.com>
2024-06-15 17:04:06 +0200

    finished the basic exercice, rest the reestructure of the project with MVC

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.get('/createdb', (req, res) => {
 })
 
 app.get('/createCategoriesTable', (req, res) => {
-    const sql = 'CREATE TABLE categories(id int AUTO_INCREMENT,categorie_name VARCHAR(255), PRIMARY KEY(id))'
+    const sql = 'CREATE TABLE categories(id int AUTO_INCREMENT,category_name VARCHAR(255), PRIMARY KEY(id))'
     db.query(sql, (err, result) => {
         if (err) throw err
         console.log(result)
@@ -38,13 +38,13 @@ app.get('/createProductsTable', (req, res) => {
 })
    
 app.post('/newCategory', (req, res) => {
-    const {categorie_name} = req.body
-    const sql = `INSERT INTO categories (categorie_name) values ('${categorie_name}');`
+    const {category_name} = req.body
+    const sql = `INSERT INTO categories (category_name) values ('${category_name}');`
    
     db.query(sql, (err, result) => {
       if (err) throw err
       console.log(result)
-      res.status(200).send('Categorie added...')
+      res.status(200).send('Category added...')
     })
 })
 
@@ -62,8 +62,8 @@ app.post('/newProduct', (req, res) => {
 
 app.put('/categories/id/:id', (req, res) => {
     const id = req.params.id  
-    const categorie_name = req.body.categorie_name
-    const newCatName = `UPDATE categories SET categorie_name = '${categorie_name}' WHERE id = ${id}`
+    const category_name = req.body.category_name
+    const newCatName = `UPDATE categories SET category_name = '${category_name}' WHERE id = ${id}`
     db.query(newCatName, (err, result) => {
         if (err) throw err
         res.send(result)
@@ -79,9 +79,109 @@ app.put('/products/id/:id', (req, res) => {
         res.send(result)
     })
 })
-        
+
+app.get('/showProducts', (req, res) => {
+    const sql = 'SELECT * FROM products'
+    db.query(sql, (err, result) => {
+      if (err) throw err;
+      res.status(200).json(result)
+    });
+  });
 
 
 
+app.get('/showCategories', (req, res) => {
+    const sql = 'SELECT * FROM categories'
+    db.query(sql, (err, result) => {
+      if (err) throw err;
+      res.status(200).json(result)
+    })
+  })
+
+  app.get('/getWithCategory', (req, res) => {
+    const sql = `
+    SELECT products.product_name, products.description, categories.category_name 
+    FROM products
+    INNER JOIN categories ON products.categories_id = categories.id`;
+  
+    db.query(sql, (err, result) => {
+      if (err) throw err;
+      if (result.length === 0) {
+        res.status(404).send('No se encontraron productos');
+      } else {
+        res.status(200).json(result);
+      }
+    });
+  });
+
+app.get('/productsById/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const sql = `SELECT * FROM products WHERE id = ${id}`
+    db.query(sql, (err, result) => {
+      if (err) throw err;
+      if (result.length === 0) {
+        res.status(404).send('Producto no encontrado')
+      } else {
+        res.status(200).json(result)
+      }
+    });
+  });
+  
+  app.get('/productsInvert', (req, res) => {
+    const sql = `
+    SELECT * FROM Products
+    ORDER BY id DESC
+    `  
+    db.query(sql, (err, result) => {
+      if (err) throw err;
+      if (result.length === 0) {
+        res.status(404).send('No se encontraron productos')
+      } else {
+        res.status(200).json(result)
+      }
+    })
+  })
+
+  app.get('/categories/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const sql = `SELECT * FROM categories WHERE id = ${id}`
+    db.query(sql, (err, result) => {
+      if (err) throw err;
+      if (result.length === 0) {
+        res.status(404).send('Categoría no encontrada')
+      } else {
+        res.status(200).json(result)
+      }
+    })
+  })
+  
+  app.get('/productsByName/:productName', (req, res) => {
+    const productName = req.params.productName;
+    const sql = `
+      SELECT p.product_name, p.description, p.price
+      FROM products p
+      JOIN categories c ON p.categories_id = c.id
+      WHERE p.product_name = '${productName}'
+    `
+    db.query(sql, (err, result) => {
+      if (err) throw err;
+     
+        res.status(200).json(result)
+      }
+    )
+  }) 
+
+  app.delete('/products/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const sql = `DELETE FROM products WHERE id = ${id}`;
+    db.query(sql, (err, result) => {
+      if (err) throw err;
+      if (result === 0) {
+        res.status(404).send('Producto no encontrado');
+      } else {
+        res.status(200).send('Producto eliminado');
+      }
+    });
+  });
 
 app.listen(PORT, () => console.log(`server started at port ${PORT}`))
